52b5ff9f14e2ad2b32aa141af71f13792ee9b093
Erick El nino <erick.tshimbombo@yahoo.com>
2022-10-22 06:41:48 +0200

    initialized rootviewcontroller and configured the Homeviewcontroller and conversationviewcontroller

---
+++ SocialChatApp/Controller/ConversationController.swift
@@ -0,0 +1,9 @@
+//
+//  ConversationController.swift
+//  SocialChatApp
+//
+//  Created by Erick El nino on 2022/10/22.
+//  Copyright © 2022 Erick El nino. All rights reserved.
+//
+
+import Foundation
